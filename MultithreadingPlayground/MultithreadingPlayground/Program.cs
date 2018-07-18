a3cad626d67414fec30cc52b123c6f73ef2ad152
robert <robson081192@gmail.com>
2018-07-18 21:37:51 +0200

    Create playground solution (starting point)

---
+++ MultithreadingPlayground/MultithreadingPlayground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultithreadingPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
